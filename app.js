d532f90d266304d57a929f8462b9a75310979b64
xiangjianlong <1415254329@qq.com>
2021-09-16 16:36:12 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const email = require('./user/email/email.js')
+email();
+
+// Routes
+app.get(`/`, (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/user', (req, res) => {
+  res.send([
+    {
+      title: 'serverless framework',
+      link: 'https://serverless.com'
+    }
+  ])
+})
+
+app.get('/user/:id', (req, res) => {
+  const id = req.params.id
+  res.send({
+    id: id,
+    title: 'serverless framework',
+    link: 'https://serverless.com'
+  })
+})
+
+app.get('/404', (req, res) => {
+  res.status(404).send('404')
+})
+
+app.get('/500', (req, res) => {
+  res.status(500).send('Server Error')
+})
+
+// Error handler
+app.use(function(err, req, res, next) {
+  console.error(err)
+  res.status(500).send('Internal Serverless Error')
+})
+
+app.listen(9000, () => {
+  console.log(`Server start on http://localhost:9000`);
+})
---
+++ user/email/email.js
@@ -0,0 +1,36 @@
+var nodemailer = require('nodemailer');
+const schedule = require('node-schedule');
+const axios = require('axios')
+let myEmail = '1415254329@qq.com'
+var transporter = nodemailer.createTransport({
+    service: 'qq',//163、qq等
+    auth: {
+        user: myEmail,//邮箱
+        pass: 'koubtmxktzbwhaia'//邮箱密码或授权码
+    }
+});
+module.exports = () => {
+    schedule.scheduleJob('20 5 * * *', function () {
+        let nowDate = new Date();
+        let gap = parseInt((Math.round(new Date()) - Date.parse('2020-02-10')) / 86400000)
+        let dateString = `今天是${nowDate.getFullYear()}年${nowDate.getMonth() + 1}月${nowDate.getDate()}日 星期${nowDate.getDay() ? nowDate.getDay() : '天'} 已经认识了${gap}天 `
+        axios.get("https://chp.shadiao.app/api.php").then(res => {
+            var mailOptions = {
+                from: myEmail,//发送者
+                to: '2967661904@qq.com',//接受者
+                subject: '给小章的',//主题名
+                html: `
+                <h1>${dateString}</h1>
+                <h2>${res.data}</h2>
+                `,//内容,
+            }
+            transporter.sendMail(mailOptions, function (err, info) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log("发送成功");
+            });
+        })
+    });
+}
